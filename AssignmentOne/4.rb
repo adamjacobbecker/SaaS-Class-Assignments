867b7add23cf126ad24ab16fd32e8458aa19b83b
adamjacobbecker <ad@mbecker.cc>
2012-02-26 15:28:07 -0800

    solution for #4

---
+++ AssignmentOne/4.rb
@@ -0,0 +1,42 @@
+class Dessert
+  
+  attr :name, true
+  attr :calories, true
+    
+  def initialize(name, calories)
+    self.name = name
+    self.calories = calories
+  end
+  
+  def healthy?
+    return true if self.calories < 200
+  end
+  
+  def delicious?
+    return true
+  end
+  
+end
+
+class JellyBean < Dessert
+  
+  attr :flavor, true
+  
+  def initialize(name, calories, flavor)
+    self.flavor = flavor
+    super(name, calories)
+  end
+  
+  def delicious?
+    return true unless flavor == "black licorice"
+  end
+  
+end
+
+# dessert = Dessert.new('ada',200)
+# p dessert.healthy?
+# p dessert.delicious?
+
+# jellybean = JellyBean.new("black", 10, "black licaorice")
+# p jellybean.healthy?
+# p jellybean.delicious?
