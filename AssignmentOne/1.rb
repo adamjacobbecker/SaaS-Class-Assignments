b68fdf884a2ef11a069809171d6024a44fc3e415
adamjacobbecker <ad@mbecker.cc>
2012-02-26 13:17:24 -0800

    initial commit

---
+++ AssignmentOne/1.rb
@@ -0,0 +1,11 @@
+def palindrome?(string)
+  if string.downcase.gsub(/\W/, '') == string.reverse.downcase.gsub(/\W/, '')
+    return true
+  end
+end
+
+String.class_eval do
+  def palindrome?
+    return true if self.downcase.gsub(/\W/, '') == self.reverse.downcase.gsub(/\W/, '')
+  end
+end
---
+++ AssignmentOne/2.rb
@@ -0,0 +1,6 @@
+def count_words(string)
+  word_count = Hash.new(0)
+  input_words = string.downcase.scan(/\w+/)
+  input_words.each {|x| word_count[x] += 1 }
+  return word_count
+end
