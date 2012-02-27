bbe84e60c7af923a2eece1b1bf2c415bda6d641c
adamjacobbecker <ad@mbecker.cc>
2012-02-26 17:07:20 -0800

    adding assignment 1 part 5

---
+++ AssignmentOne/5.rb
@@ -0,0 +1,40 @@
+class Class  
+  
+  def attr_accessor_with_history(attr_name)
+    attr_name = attr_name.to_s  # make sure it's a string
+    attr_reader attr_name  # create the attribute's getter
+    attr_reader attr_name+"_history" # create bar_history getter
+
+    #class_eval "your code here, use %Q for multiline strings"
+    class_eval do
+      
+      def initialize
+         @history = Array.new().push(nil)
+      end
+            
+      define_method(attr_name + "=" ) do |x|
+        @history.push(x)
+      end
+      
+      define_method(attr_name + "_history") do
+        return @history
+      end
+      
+    end
+    
+  end
+  
+end
+
+class Foo
+  attr_accessor_with_history :bar
+end
+
+=begin
+f = Foo.new
+f.bar = 1
+f.bar = 2
+f = Foo.new
+f. bar = 4
+p f.bar_history
+=end
