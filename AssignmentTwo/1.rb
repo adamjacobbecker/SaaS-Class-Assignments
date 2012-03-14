79e8e827f6bcc0f40c5adcc79e3d010fda2bd50b
adamjacobbecker <ad@mbecker.cc>
2012-03-14 11:18:33 -0700

    Assignment Two / Part 1

---
+++ AssignmentTwo/1.rb
@@ -0,0 +1,40 @@
+class Numeric
+  @@currencies = {'dollar' => 1, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  def method_missing(method_id, *args)
+    singular_currency = method_id.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self * (@@currencies[singular_currency] / @@currencies['dollar'])
+    elsif method_id.to_s == 'in'
+      singular_currency = *args[0].to_s.gsub( /s$/, '')
+      if @@currencies.has_key?(singular_currency)
+        self * (@@currencies['dollar'] / @@currencies[singular_currency])
+      end
+    else
+      super
+    end
+  end
+end
+
+#p 12.euros.in(:dollars)
+#p 12.yen
+#p 12.euro.in(:dollar)
+#p 12.rupees.in(:dollar)
+#p 12.rupee.in(:dollars)
+
+class String
+  def palindrome?
+    true if self.downcase.gsub(/\W/, '') == self.reverse.downcase.gsub(/\W/, '')
+  end
+end
+
+class Array
+  def palindrome?
+    true if self.to_s.palindrome?
+  end
+end
+
+#p "ooo".palindrome?
+#p "foo".palindrome?
+
+#p [1,2,3,2,1].palindrome?
+#p [1,2,3,2,2].palindrome?
